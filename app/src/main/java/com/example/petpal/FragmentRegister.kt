9f1b1af4bdf6381ba47a91d20962bdc03bbdeed7
Danilo Potpara 17315 <danilopotpara@elfak.rs>
2022-05-24 21:48:18 +0200

    Odradjen skelet register komponente

--- app/src/main/java/com/example/petpal/FragmentRegister.kt
+++ app/src/main/java/com/example/petpal/FragmentRegister.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.petpal.databinding.FragmentRegisterBinding
 
 //pocistio junk code
 
 class FragmentRegister : Fragment() {
 
+    private lateinit var binding: FragmentRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -18,7 +21,37 @@ class FragmentRegister : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentRegisterBinding.inflate(layoutInflater)
+        return binding.root
+    }
 
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val spin = binding.dropdownMenuDogBreed
+        context?.let {
+            ArrayAdapter.createFromResource(
+                it,
+                R.array.dogBreeds,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                // Apply the adapter to the spinner
+                spin.adapter = adapter
+            }
+        }
+        val spin2 = binding.dropdownMenuDogStatus
+        context?.let {
+            ArrayAdapter.createFromResource(
+                it,
+                R.array.dogStatus,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                // Apply the adapter to the spinner
+                spin2.adapter = adapter
+            }
+        }
     }
 }
