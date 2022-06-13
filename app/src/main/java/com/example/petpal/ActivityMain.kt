7cb9e8dad41afdca2f06d08ca1ae582348349619
nstoj2000 <nemanjastojanovic2000@gmail.com>
2022-06-13 22:11:15 +0200

    Dodata mapa u MapFragment Popravljen bag koji je izazivao crashove sa Progress Dialogom

--- app/src/main/java/com/example/petpal/ActivityMain.kt
+++ app/src/main/java/com/example/petpal/ActivityMain.kt
@@ -8,6 +8,5 @@ class ActivityMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        actionBar
     }
 }
